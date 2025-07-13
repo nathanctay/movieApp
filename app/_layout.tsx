db20e12b6c4cf0d27bebc18bc341dfa0f599c37e
nathanctay <63444651+nathanctay@users.noreply.github.com>
2025-07-13 17:17:05 -0600

    just added backgrounds to profile and saved page. Will implement later

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout() {
           }}
         />
         <Stack.Screen
-          name="movie/[id]"
+          name="movies/[id]"
           options={{
             headerShown: false,
           }}
