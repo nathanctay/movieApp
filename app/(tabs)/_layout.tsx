ab0350824741a7b69de7fc58364a0e8e1b94d486
nathanctay <63444651+nathanctay@users.noreply.github.com>
2025-07-13 12:17:30 -0600

    homescreen search bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ focused, icon, title }) => {
         return (
             <ImageBackground
                 source={images.highlight}
-                className='flex flex-row w-full flex-1 min-w-[94px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-[3.75rem] mt-4 justify-center items-center rounded-full overflow-hidden'
             >
                 <Image
                     source={icon}
